9cd7bcf7fe2624622cadfda353752315d6ecd40e
Andrew G <ck3908@gmail.com>
2013-10-05 02:10:56 -0400

    bundle install twilio api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instantwilio::Application.config.secret_key_base = '0f1ba70ce038db247524075c1d2e46ed3e6e05714fe4618e3963864267bf5e81618261fc1250c626378d891b9a9125fed87dce5658b290c14687492c2d617edc'
